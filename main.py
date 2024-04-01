8718a5beef6b84ba266de1bf609e1ce0d994a46c
katiaviene <katiacookiefairy@gmail.com>
2024-04-01 15:54:46 +0300

    more queries

--- main.py
+++ main.py
@@ -70,19 +70,90 @@ if __name__ == "__main__":
     print(read_query(query, conn))
     
     # 4. How many times did customers switch from a less expensive to a more expensive subscription?
-    
+    # Swich - the same date, 
     query = """
-            SELECT *
+            SELECT 
+            count(*)
+            
             FROM (
             SELECT
             *, 
             LEAD(rate) over (partition by subscriber_id order by effective_date) as next_rate,
             LEAD(effective_date) over(partition by subscriber_id order by effective_date) as next_eff_date
             FROM subscriptions) sub
-            where sub.next_rate > sub.rate and julianday(sub.next_eff_date)-  julianday(sub.expiration_date) <=1
-    
+            where sub.next_rate > sub.rate and julianday(sub.next_eff_date) -  julianday(sub.expiration_date) <=1
+            and sub.rate != 0
     """
     print(read_query(query, conn))
     
+    
+    # 5. Which week of which year did the majority of subscriptions expire?
+    query = """
+            SELECT
+            strftime('%Y', expiration_date) as year,
+            strftime('%W', expiration_date) + 1 as week,
+            count(subscriber_id) as cnt
+            from subscriptions
+            group by 
+            strftime('%Y', expiration_date),
+            strftime('%W', expiration_date)
+            Order by count(subscriber_id) desc
+            LIMIT 1
+    """
+    print(read_query(query, conn))
+    
+    # 6.How many new customers have been added on 2018-12-12? How many existing customers renewed their subscriptions on 2018-12-12?
+    query = """
+        SELECT 
+        CASE
+         WHEN before_date is null then 'new_customer'
+         ELSE 'existing_customer'
+        END,
+        count(subscriber_id) as count
+        FROM (
+         SELECT *,
+         lag(effective_date) over (partition by subscriber_id order by effective_date) as before_date
+         FROM subscriptions ) sub
+         WHERE date(effective_date) = '2018-12-12'
+        GROUP BY 
+            CASE
+         WHEN before_date is null then 'new_customer'
+         ELSE 'existing_customer'
+        END
+        
+    """
+    print(read_query(query, conn))
+    
+    # 7. Every week of every year lists the most expensive subscription, its number, segment, and rate.
+    query = """
+            SELECT 
+            DISTINCT
+            rate.year,
+            rate.week,
+            rate.max_rate,
+            sub.soc_pp_code,
+            pp.product_segment
+            
+            
+            FROM 
+            (
+            SELECT
+            strftime('%Y', effective_date) as year,
+            strftime('%W', effective_date) + 1 as week,
+            max(rate) as max_rate
+            FROM subscriptions
+            GROUP BY 
+            strftime('%Y', effective_date),
+            strftime('%W', effective_date)) rate
+            left join 
+            subscriptions sub
+            on strftime('%Y', sub.effective_date) = rate.year
+            and strftime('%W', sub.effective_date)+1 = rate.week
+            and sub.rate = rate.max_rate
+            left join priceplan pp
+            on pp.soc_pp_code = sub.soc_pp_code
+    """
+    
+    print(read_query(query, conn))
     
     
